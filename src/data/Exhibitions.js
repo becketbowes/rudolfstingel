e454acdeea8651514443d95baabdb5ba780ed481
becket <becket.bowes@gmail.com>
2024-09-20 18:03:56 -0400

    dummy data added

---
+++ src/data/Exhibitions.js
@@ -0,0 +1,73 @@
+export const exhibitions = {
+    2014: [
+      {
+        id: '2014-1',
+        title: 'Gagosian NY 21st Street',
+        description: 'A remarkable exhibition showcasing contemporary art at Gagosian Gallery, New York.',
+        images: [
+          '/assets/exhibitionImages/2014/gagosianNY21st/image1.jpg',
+          '/assets/exhibitionImages/2014/gagosianNY21st/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/gagosian-ny-21st',
+        externalLink: 'https://gagosian.com/exhibitions/new-york-21st-street-2014'
+      },
+      {
+        id: '2014-2',
+        title: 'Massimo De Carlo Milan via Ventura',
+        description: 'A major exhibition hosted by Massimo De Carlo in Milan, showcasing modern Italian art.',
+        images: [
+          '/assets/exhibitionImages/2014/massimoDeCarlo/image1.jpg',
+          '/assets/exhibitionImages/2014/massimoDeCarlo/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/massimo-de-carlo-milan',
+        externalLink: 'https://massimodecarlo.com/exhibitions/milan-via-ventura-2014'
+      },
+      {
+        id: '2014-3',
+        title: 'Paula Cooper NY',
+        description: 'A captivating exhibition held at Paula Cooper Gallery, highlighting abstract works.',
+        images: [
+          '/assets/exhibitionImages/2014/paulaCooperNY/image1.jpg',
+          '/assets/exhibitionImages/2014/paulaCooperNY/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/paula-cooper-ny',
+        externalLink: 'https://paulacoopergallery.com/exhibitions/new-york-2014'
+      }
+    ],
+    2015: [
+      {
+        id: '2015-1',
+        title: 'Gagosian Hong Kong',
+        description: 'An exciting exhibition at Gagosian Gallery in Hong Kong, exploring Eastern influences.',
+        images: [
+          '/assets/exhibitionImages/2015/gagosianHongKong/image1.jpg',
+          '/assets/exhibitionImages/2015/gagosianHongKong/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/gagosian-hong-kong',
+        externalLink: 'https://gagosian.com/exhibitions/hong-kong-2015'
+      },
+      {
+        id: '2015-2',
+        title: 'Gagosian NY Park & 75th - Part I',
+        description: 'An influential exhibition in New York showcasing Part I of the series at Gagosian Park & 75th.',
+        images: [
+          '/assets/exhibitionImages/2015/gagosianPark75th/image1.jpg',
+          '/assets/exhibitionImages/2015/gagosianPark75th/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/gagosian-ny-park-part1',
+        externalLink: 'https://gagosian.com/exhibitions/new-york-park-and-75th-2015'
+      },
+      {
+        id: '2015-3',
+        title: 'Sadie Coles London Davies St & Kingly St',
+        description: 'A dynamic exhibition held across both Sadie Coles galleries in London.',
+        images: [
+          '/assets/exhibitionImages/2015/sadieColesLondon/image1.jpg',
+          '/assets/exhibitionImages/2015/sadieColesLondon/image2.jpg'
+        ],
+        pressRelease: 'https://example.com/press-release/sadie-coles-london',
+        externalLink: 'https://sadiecoles.com/exhibitions/london-davies-street-kingly-street-2015'
+      }
+    ]
+  };
+  
