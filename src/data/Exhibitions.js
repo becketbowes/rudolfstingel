58195e997671b664398bbeb9508e347e92e6e1d6
becket <becket.bowes@gmail.com>
2024-10-01 16:12:14 -0400

    file folder restructuring

--- src/data/Exhibitions.js
+++ src/data/Exhibitions.js
@@ -21,55 +21,55 @@ export const exhibitions = {
         "description": "A remarkable exhibition showcasing contemporary art at Gagosian Gallery, New York.",
         "images": [
         {
-            "path": "assets/images/shows/2014GagosianNYe1.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe1.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe2.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe2.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe3.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe3.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe4.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe4.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe5.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe5.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe6.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe6.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNYe7.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNYe7.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014GagosianNY664_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY664_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 131 1/2 x 180 1/2 in (334 x 458.4 cm)"
         },
         {
-            "path": "assets/images/shows/2014GagosianNY666_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY666_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 132 x 181 3/4 in (335.5 x 456.5 cm)"
         },
         {
-            "path": "assets/images/shows/2014GagosianNY667_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY667_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 131 7/8 x 179 7/8 in (334.7 x 456.7 cm)"
         },
         {
-            "path": "assets/images/shows/2014GagosianNY668_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY668_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 132 x 180 3/4 in (335.3 x 459 cm)"
         },
         {
-            "path": "assets/images/shows/2014GagosianNY669_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY669_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 132 x 180 3/4 in (335.3 x 459 cm)"
         },
         {
-            "path": "assets/images/shows/2014GagosianNY1672_RI_JL.jpg",
+            "path": "assets/images/shows/2014/2014GagosianNY1672_RI_JL.jpg",
             "text": "Untitled, 2009, oil on canvas, 132 x 181 in (335.3 x 459.7 cm)"
         }
         ],
@@ -81,63 +81,63 @@ export const exhibitions = {
         "description": "A major exhibition hosted by Massimo De Carlo in Milan, showcasing modern Italian art.",
         "images": [
         {
-            "path": "assets/images/shows/2014MDeCarloMLe1.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe1.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe2.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe2.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe3.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe3.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe4.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe4.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe5.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe5.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe6.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe6.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe7.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe7.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe8.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe8.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloMLe9.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloMLe9.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1537.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1537.jpg",
             "text": "Untitled, 2014, Electroformed copper, plated nickel, stainless steel frame, 94 1/2 x 189 x 1 1/2 in (240 x 480 x 4 cm) 8 panels"
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1705_RI_JL.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1705_RI_JL.jpg",
             "text": "Untitled, 2014, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1706_RI_JL.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1706_RI_JL.jpg",
             "text": "Untitled, 2014, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1732.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1732.jpg",
             "text": "Untitled, 2014, Electroformed copper, plated nickel, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1734.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1734.jpg",
             "text": "Untitled, 2014, Electroformed copper, plated nickel, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
         },
         {
-            "path": "assets/images/shows/2014MDeCarloML1757_RI_JL.jpg",
+            "path": "assets/images/shows/2014/2014MDeCarloML1757_RI_JL.jpg",
             "text": "Untitled, 2014, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
         }
         ],
@@ -149,15 +149,15 @@ export const exhibitions = {
         "description": "A captivating exhibition held at Paula Cooper Gallery, highlighting abstract works.",
         "images": [
         {
-            "path": "assets/images/shows/2014PCooperNYe1.jpg",
+            "path": "assets/images/shows/2014/2014PCooperNYe1.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014PCooperNYe2.jpg",
+            "path": "assets/images/shows/2014/2014PCooperNYe2.jpg",
             "text": null
         },
         {
-            "path": "assets/images/shows/2014PCooperNY1804_RI_JL.jpg",
+            "path": "assets/images/shows/2014/2014PCooperNY1804_RI_JL.jpg",
             "text": "Untitled, 2014, Oil on canvas, 176 x 120 in (447 x 304.8 cm)"
         } 
         ],
@@ -171,67 +171,67 @@ export const exhibitions = {
         "description": "A striking exhibition hosted by Gagosian Gallery in Hong Kong, featuring contemporary works.",
         "images": [
           {
-            "path": "assets/images/shows/2015GagosianHKe1.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe1.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe2.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe2.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe3.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe3.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe4.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe4.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe5.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe5.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe6.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe6.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe7.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe7.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHKe8.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHKe8.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1825_0019.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1825_0019.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 94 1/2 x 94 1/2 x 1 1/2 in (240 x 240 x 4 cm); 4 panels"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1827_0003.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1827_0003.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 94 1/2 x 94 1/2 x 1 1/2 in (240 x 240 x 4 cm); 4 panels"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1828_0003.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1828_0003.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 94 1/2 x 94 1/2 x 1 1/2 in (240 x 240 x 4 cm); 4 panels"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1832_0007.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1832_0007.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1833_0011_D.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1833_0011_D.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1834_0003.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1834_0003.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1839_0003.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1839_0003.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel, 47 1/4 x 47 1/4 x 1 1/2 in (120 x 120 x 4 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianHK1841_0003.jpg",
+            "path": "assets/images/shows/2015/2015GagosianHK1841_0003.jpg",
             "text": "Untitled, 2012, Electroformed copper, plated nickel and gold, stainless steel 47 1/4 x 43 1/8 x 1 1/2 in (120 x 109.6 x 4 cm)"
           }
         ],
@@ -243,43 +243,43 @@ export const exhibitions = {
         "description": "An exciting exhibition held at Gagosian Gallery in New York, Park & 75th.",
         "images": [
           {
-            "path": "assets/images/shows/2015GagosianNYe1.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe1.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNYe2.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe2.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNYe3.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe3.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNYe4.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe4.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNYe5.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe5.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNYe6.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNYe6.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015GagosianNY1301_JL.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNY1301_JL.jpg",
             "text": "Untitled (Franz West), 2010, Ink, oil and enamel on paper, 30 x 22 1/4 in (76.2 x 56.5 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianNY1469_JL.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNY1469_JL.jpg",
             "text": "Untitled, 2012, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianNY1470_JL.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNY1470_JL.jpg",
             "text": "Untitled, 2012, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
           },
           {
-            "path": "assets/images/shows/2015GagosianNY1471_RI_JL.jpg",
+            "path": "assets/images/shows/2015/2015GagosianNY1471_RI_JL.jpg",
             "text": "Untitled, 2012, Oil and enamel on canvas, 95 x 76 in (241.3 x 193 cm)"
           }
         ],
@@ -291,43 +291,43 @@ export const exhibitions = {
         "description": "An exhibition at Sadie Coles HQ in London, that ran concurrently with the Davies Street location",
         "images": [
           {
-            "path": "assets/images/shows/2015SColesLOe5.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe5.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe6.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe6.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe7.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe7.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe8.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe8.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe9.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe9.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe10.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe10.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLO1823_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1823_JL.jpg",
             "text": "Untitled, 2014, Oil on canvas, 96 x 96 in (243.8 x 243.8 cm)"
           },
           {
-            "path": "assets/images/shows/2015SColesLO1914_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1914_JL.jpg",
             "text": "Untitled, 2015, Oil on canvas, 96 x 96 in (243.8 x 243.8 cm)"
           },
           {
-            "path": "assets/images/shows/2015SColesLO1957_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1957_JL.jpg",
             "text": "Untitled, 2015, Oil on canvas, 96 x 96 in (243.8 x 243.8 cm)"
           },
           {
-            "path": "assets/images/shows/2015SColesLO1958_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1958_JL.jpg",
             "text": "Untitled, 2015, Oil on canvas, 96 x 96 in (243.8 x 243.8 cm)"
           }
         ],
@@ -339,31 +339,31 @@ export const exhibitions = {
         "description": "An exhibition at Sadie Coles HQ in London that ran concurrently with the Kingly Street location",
         "images": [
           {
-            "path": "assets/images/shows/2015SColesLOe1.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe1.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe2.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe2.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe3.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe3.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLOe4.jpg",
+            "path": "assets/images/shows/2015/2015SColesLOe4.jpg",
             "text": null
           },
           {
-            "path": "assets/images/shows/2015SColesLO1913.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1913.jpg",
             "text": "Untitled, 2015, Oil on canvas, 96 x 96 in (243.8 x 243.8 cm)"
           },
           {
-            "path": "assets/images/shows/2015SColesLO1818_RI_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1818_RI_JL.jpg",
             "text": "Untitled, 2014, Oil on canvas, 50 x 50 in (127 x 127 cm)"
           },
           {
-            "path": "assets/images/shows/2015SColesLO1980_JL.jpg",
+            "path": "assets/images/shows/2015/2015SColesLO1980_JL.jpg",
             "text": "Untitled, 2015, Oil on canvas, 50 x 50 in (127 x 127 cm)"
           }
         ],
