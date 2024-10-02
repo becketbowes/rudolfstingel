03524feb54f44aebb695ca2a12a9014b5df7dfbc
becket <becket.bowes@gmail.com>
2024-10-02 11:43:55 -0400

    2106 half added

--- src/data/Exhibitions.js
+++ src/data/Exhibitions.js
@@ -370,7 +370,328 @@ export const exhibitions = {
         "externalLink": "sadiecoles.com/exhibitions/175-rudolf-stingel/installation_shots/"
       }
   ],
-    2016: [],
+    2016: [
+      {
+        "id": "2016-1",
+        "title": "Gagosian New York - Part II",
+        "description": "The second part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_e1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_e2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_1352_RI_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_1423_RI_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_1426_RI_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_II_1427_RI_JL.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-ii"
+      },
+      {
+        "id": "2016-2",
+        "title": "Gagosian New York - Part III",
+        "description": "The third part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IIIe1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IIIe2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IIIe3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IIIe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IIIe5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_III_1807_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_III_1856_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_III_1857_JL.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-iii"
+      },
+      {
+        "id": "2016-3",
+        "title": "Gagosian New York - Part IV",
+        "description": "The fourth part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IVe1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IVe2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IVe3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IVe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_IV_1907_JL.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-iv"
+      },
+      {
+        "id": "2016-4",
+        "title": "Gagosian New York - Part V",
+        "description": "The fifth part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_Ve1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_Ve2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_Ve3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_Ve4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_Ve5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_V_1519.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_V_2082.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_V_2083.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_V_2084.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-v"
+      },
+      {
+        "id": "2016-5",
+        "title": "Gagosian New York - Part VI",
+        "description": "The sixth part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIe5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VI_2042.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VI_2043.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VI_2045.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VI_2046.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VI_2047.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-vi"
+      },
+      {
+        "id": "2016-6",
+        "title": "Gagosian New York - Part VII",
+        "description": "The seventh part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIe1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIe2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIe3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIe5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VII_2127.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VII_2128.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VII_2129.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VII_2130_JL.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-vii"
+      },
+      {
+        "id": "2016-7",
+        "title": "Gagosian New York - Part VIII",
+        "description": "The eighth part of the exhibition held at Gagosian New York Park & 75th.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIIe1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIIe2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIIe3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIIe4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIIIe5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIII_228_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIII_2140_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIII_2141_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016GagPark&75_VIII_2142_JL.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/gagosian-ny-part-viii"
+      },
+      {
+        "id": "2016-8",
+        "title": "Massimo De Carlo Palazzo Belgioioso",
+        "description": "Exhibition at Palazzo Belgioioso hosted by Massimo De Carlo.",
+        "images": [
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_e1.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_e2.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_e3.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_e4.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_e5.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_1729_JL.jpg",
+            "text": null
+          },
+          {
+            "path": "assets/images/shows/2016/2016_MDeCarloPalazzoBelgioioso_2119.jpg",
+            "text": null
+          }
+        ],
+        "externalLink": "https://example.com/external-link/massimo-de-carlo-palazzo-belgioioso"
+      }
+    ],
     2017: [],
     2018: [],
     2019: [],
