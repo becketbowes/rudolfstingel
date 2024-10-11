4f361bea6082072943371d96d4121728ca12010e
becket <becket.bowes@gmail.com>
2024-10-11 15:25:28 -0400

    readjusted UX to have stable title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -23,16 +23,6 @@ function Header() {
     }
   }, [isHorizontal]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-    };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   const handleReset = () => {
     setSelectedYear(null);
     setSelectedExhibition(null);
@@ -41,7 +31,7 @@ function Header() {
   };
 
   return (
-    <header role="banner">
+    <header role="banner" className="fixed-header">
       <button 
         className="reset-button" 
         onClick={handleReset} 
@@ -64,59 +54,3 @@ function Header() {
 }
 
 export default Header;
-
-// import React, { useState, useEffect } from 'react';
-// import { useAppContext } from '../../context/AppContext';
-// import './Header.css';
-
-// function Header() {
-//   const [titleState, setTitleState] = useState('initial');
-//   const { setSelectedYear, setSelectedExhibition, isHorizontal, setIsHorizontal } = useAppContext();
-
-//   useEffect(() => {
-//     setTitleState('start')
-//     const timer = setTimeout(() => {
-//       setTitleState('animated');
-//     }, 100);
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   useEffect(() => {
-//     if (isHorizontal) {
-//       setTitleState('horizontal-menu');
-//     } else {
-//       setTitleState('animated');
-//     }
-//   }, [isHorizontal]);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-//     };
-
-//     handleResize();
-//     window.addEventListener('resize', handleResize);
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   const handleReset = () => {
-//     setSelectedYear(null);
-//     setSelectedExhibition(null);
-//     setIsHorizontal(false);
-//     setTitleState('animated');
-//   };
-
-//   return (
-//     <>
-//       <button className="reset-button" onClick={handleReset} aria-label="Reset">
-//         <span className="sr-only">Reset</span>
-//       </button>
-//       <h1 className={`title ${titleState}`}>
-//         Rudolf Stingel
-//       </h1>
-//     </>
-//   );
-// }
-
-// export default Header;
